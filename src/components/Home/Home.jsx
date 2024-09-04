dc6cd66895b7cb18199d9240c91c2998e9ae4e74
Mashnur <221007712@eastdelta.edu.bd>
2024-09-04 12:32:01 +0600

    added navigation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header/Header";
 import Footer from "../Footer/Footer";
 
 
 const Home = () => {
+    const navigation =useNavigation();
+
     return (
         <div>
             <Header></Header>
             <h2 className="bg-orange-400 font-extrabold">This is the homePage</h2>
+           {
+            navigation.state=== "loading" ?
+        <p>Loading...</p>:
             <Outlet></Outlet>
+        
+           
+           }
+           
             <Footer></Footer>
            
             
